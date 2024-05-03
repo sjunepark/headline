41c33979e9687760f1a39c3516b4103e1c5b92ef
June Park <junepark202012@gmail.com>
2024-05-04 08:17:24 +0900

    feat: modify ScrapeStatus

--- internal/pkg/model/article.go
+++ internal/pkg/model/article.go
@@ -1,14 +1,35 @@
 package model
 
-import "net/url"
+import (
+	"net/url"
+	"time"
+)
 
 type Article struct {
 	Title           string
-	CreatedDateTime string // format: "2006-01-02 15:04:05"
-	UpdatedDateTime string // format: "2006-01-02 15:04:05"
+	CreatedDateTime time.Time
+	UpdatedDateTime time.Time
 	Source          string
-	Url             url.URL
+	Url             *url.URL
 	Summary         string
 	Content         string
 	Keywords        map[string]bool
+	ScrapeStatus    ScrapeStatus
+}
+
+// ScrapeStatus is an enum for the status of scraping process
+// 1. UrlScraped: the url has been scraped, which would be from scraping the list of articles
+// 2. ListItemParsed: the list item element has been parsed,
+// which would usually have parsed the title, url, and datetime, etc.
+// 3. ArticleParsed: the article has been parsed, which would have parsed the text content
+type ScrapeStatus int
+
+const (
+	UrlScraped ScrapeStatus = iota
+	ListItemParsed
+	ArticleParsed
+)
+
+func (a *Article) IsUrlValid() bool {
+	return a.Url.Scheme != "" && a.Url.Host != ""
 }
