7ce95a0893c90a487bff5999325a50f83606aa22
June Park <junepark202012@gmail.com>
2024-05-06 19:49:28 +0900

    feat: change browserOptions depending on env

--- internal/pkg/scraper/util.go
+++ internal/pkg/scraper/util.go
@@ -3,11 +3,31 @@ package scraper
 import (
 	"github.com/sejunpark/headline/internal/pkg/constant"
 	"github.com/sejunpark/headline/internal/pkg/rodext"
+	"os"
 )
 
-var DefaultBrowserOptions = rodext.BrowserOptions{
-	Debug:           false,
-	NoDefaultDevice: true,
-	Incognito:       true,
-	PagePoolSize:    constant.PAGE_POOL_SIZE,
+func init() {
+	DefaultBrowserOptions = defaultBrowserOptions()
+}
+
+var DefaultBrowserOptions rodext.BrowserOptions
+
+func defaultBrowserOptions() rodext.BrowserOptions {
+	options := DefaultBrowserOptions
+	if os.Getenv("CI") == "true" {
+		options = rodext.BrowserOptions{
+			Debug:           true,
+			NoDefaultDevice: true,
+			Incognito:       true,
+			PagePoolSize:    constant.PAGE_POOL_SIZE,
+		}
+	} else {
+		options = rodext.BrowserOptions{
+			Debug:           false,
+			NoDefaultDevice: true,
+			Incognito:       true,
+			PagePoolSize:    constant.PAGE_POOL_SIZE,
+		}
+	}
+	return options
 }
