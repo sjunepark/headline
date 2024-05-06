41526b9a6ba8d4825c5c1d810594caf607820834
June Park <junepark202012@gmail.com>
2024-05-06 21:27:44 +0900

    test: change logLevel

--- internal/pkg/scraper/testutil/builderSuite.go
+++ internal/pkg/scraper/testutil/builderSuite.go
@@ -3,19 +3,24 @@ package testutil
 import (
 	"github.com/sejunpark/headline/internal/pkg/scraper"
 	"github.com/stretchr/testify/suite"
+	"log/slog"
 )
 
 type BaseScraperBuilderSuite struct {
 	suite.Suite
-	Builder scraper.Builder
-	cleanup func()
+	Builder  scraper.Builder
+	cleanup  func()
+	logLevel slog.Level
 }
 
 func (ts *BaseScraperBuilderSuite) SetupScraperBuilderSuite(builder scraper.Builder, cleanup func()) {
+	ts.logLevel = slog.SetLogLoggerLevel(slog.LevelDebug)
+
 	ts.Builder = builder
 	ts.cleanup = cleanup
 }
 
 func (ts *BaseScraperBuilderSuite) TearDownSuite() {
+	slog.SetLogLoggerLevel(ts.logLevel)
 	ts.cleanup()
 }
