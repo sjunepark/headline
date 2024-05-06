39f6514d7e2d17a4e88d0e828c2d4bf318ff66fe
June Park <junepark202012@gmail.com>
2024-05-07 08:30:35 +0900

    test: implement test for Element.HTML and Element.Text

--- internal/pkg/rodext/element_test.go
+++ internal/pkg/rodext/element_test.go
@@ -1,6 +1,7 @@
 package rodext
 
 import (
+	"github.com/sejunpark/headline/internal/pkg/util"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
@@ -23,7 +24,7 @@ func TestElementSuite(t *testing.T) {
 
 func (ts *ElementSuite) TestElement_Element() {
 	ts.Run("should return an Element with the correct selector", func() {
-		aElement, err := ts.ulElement.Element("a[href='#section1']")
+		aElement, err := ts.ulElement.Element("a[href='#section2']")
 		html, _ := aElement.HTML()
 		ts.NoErrorf(err, "failed to get Element, target ulElement: %v", html)
 		ts.Truef(isElement(aElement), "returned Element is not an Element, got %T", aElement)
@@ -77,6 +78,91 @@ func (ts *ElementSuite) TestElement_Attribute() {
 		emptyAttr, err := ts.ulElement.Attribute("empty")
 		ts.NoError(err, "failed to get attribute")
 		ts.Equal("", emptyAttr)
+	})
+}
+
+func (ts *ElementSuite) TestElement_Text() {
+	tt := []struct {
+		name string
+		got  string
+		want string
+	}{
+		{
+			name: "should return the text content of the element",
+			got:  ts.ulElement.Text(),
+			want: "Section 1\nSection 2\nSection 3\nSection 1",
+		},
+	}
+
+	for _, tc := range tt {
+		ts.Run(tc.name, func() {
+			text := ts.ulElement.Text()
+			ts.Equal(tc.want, text)
+		})
+
+	}
+}
+
+func (ts *ElementSuite) TestElement_HTML() {
+	tt := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "should return the HTML content of the element",
+			want: "<ul class=\"ulClass\" empty=\"\">\n        <li><a href=\"#section1\">Section 1</a></li>\n        <li><a href=\"#section2\">Section 2</a></li>\n        <li><a href=\"#section3\">Section 3</a></li>\n        <li><a href=\"#section1\">Section 1</a></li>\n    </ul>",
+		},
+	}
+
+	for _, tc := range tt {
+		ts.Run(tc.name, func() {
+			html, err := ts.ulElement.HTML()
+			ts.NoError(err, "failed to get HTML")
+			ts.Equal(tc.want, html)
+		})
+	}
+}
+
+func (ts *ElementSuite) TestElement_Equal() {
+	ts.Run("should return true if the text content of the two elements html are the same", func() {
+		firstLi, err := ts.ulElement.Element("li:nth-child(1)")
+		ts.NoError(err, "failed to get Element")
+
+		fourthLi, err := ts.ulElement.Element("li:nth-child(4)")
+		ts.NoError(err, "failed to get Element")
+
+		ts.True(firstLi.Equal(firstLi))
+		ts.True(firstLi.Equal(fourthLi))
+		ts.True(fourthLi.Equal(firstLi))
+	})
+
+	ts.Run("should return false if the text content of the two elements html are not the same", func() {
+		liSection2, err := ts.Page.Element("a[href='#section2']")
+		ts.NoError(err, "failed to get Element")
+
+		h3Section2, err := ts.Page.Element("h3#section2")
+		ts.NoError(err, "failed to get Element")
+
+		ts.False(liSection2.Equal(h3Section2))
+		ts.False(h3Section2.Equal(liSection2))
+	})
+
+	ts.Run("should return false when both elements are nil", func() {
+		var el1, el2 *Element
+		ts.False(el1.Equal(el1))
+		ts.False(el1.Equal(el2))
+		ts.False(el2.Equal(el1))
+	})
+
+	ts.Run("should return false when one of the elements is nil", func() {
+		var el *Element
+		equal, err := ts.ulElement.Equal(el)
+		ts.ErrorIs(err, util.NilError)
+		ts.False(equal)
+
+		equal, err = el.Equal(ts.ulElement)
+		ts.ErrorIs(err, util.NilError)
+		ts.False(equal)
 	})
 }
 
